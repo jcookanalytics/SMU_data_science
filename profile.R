d2bfa34097b750fcbf58e7bda7f7bda829682cec
jcookanalytics <joseph.i.cook87@gmail.com>
2016-10-05 23:11:56 -0500

    profile

---
+++ profile.R
@@ -0,0 +1,6 @@
+skill <- c("computer.programming","math","statistics","machine.learning","domain.expertise","communication.and.presentation.skills","data.visualization")
+level <- c(3,4,3,3,4,3,3)
+skill <- as.data.frame(skill)
+level <- as.data.frame(level)
+joseph <- cbind(skill,level)
+joseph
